46752c6b8a8fdd38b12bb1b404e39ce55a7f487f
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2013-10-31 18:24:58 +0300

    Added an update handler

--- app.go
+++ app.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/gorilla/mux"
 	"html/template"
+	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -22,15 +24,16 @@ func main() {
 func configureRoutes(r *mux.Router) {
 	r.HandleFunc("/{title:[a-zA-Z0-9_-]+}", ViewHandler).Methods("GET")
 	r.HandleFunc("/{title:[a-zA-Z0-9_-]+}/edit", EditHandler).Methods("GET")
-	r.HandleFunc("/{title:[a-zA-Z0-9_-]+}", ViewHandler).Methods("POST")
+	r.HandleFunc("/{title:[a-zA-Z0-9_-]+}", UpdateHandler).Methods("POST")
 }
 
 func ViewHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	title := vars["title"]
-	p := GetPage(title)
+	p, err := GetPage(title)
 	if p == nil {
-		http.Redirect(w, r, "/"+vars["title"]+"edit/", http.StatusFound)
+		log.Println(err)
+		http.Redirect(w, r, "/"+vars["title"]+"/edit", http.StatusFound)
 		return
 	}
 	//TODO: handle err
@@ -40,12 +43,22 @@ func ViewHandler(w http.ResponseWriter, r *http.Request) {
 func EditHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	title := vars["title"]
-	p := GetPage(title)
+	p, err := GetPage(title)
 	if p == nil {
+		log.Println(err)
 		p = &Page{Title: title}
 	}
 	//TODO: handle err
 	templates.ExecuteTemplate(w, "edit.html", p)
+}
+
+func UpdateHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	title := vars["title"]
+	body := r.FormValue("body")
+	ioutil.WriteFile(title, []byte(body), 0600)
+	http.Redirect(w, r, "/"+vars["title"], http.StatusFound)
+	return
 }
 
 type Page struct {
@@ -53,8 +66,12 @@ type Page struct {
 	Body  []byte
 }
 
-func GetPage(title string) *Page {
-	return &Page{Title: title}
+func GetPage(title string) (*Page, error) {
+	bytes, err := ioutil.ReadFile(title)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: bytes}, nil
 }
 
 //type PageStore interface {
